66d003c92aae76adf7d8e3841eb4dba2eb017cf5
PrasadPawar2729 <prasadpawarpp2002@gmail.com>
2024-07-22 21:56:06 +0530

    mew

--- server/index.js
+++ server/index.js
@@ -13,7 +13,14 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:5173',  // Update this to match your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  
+  app.use(cors(corsOptions));
+
 app.use("/user",userRouter)
 app.use("/product",auth,productRouter)
 app.use("/",(req,res)=>{
